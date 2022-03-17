486cd20b2d50d6fd4280bc19e9db5b4237af2888
Munawar <munawar.hassan@in.ibm.com>
2022-03-17 21:58:57 +0530

    sql import file updated

--- src/main/resources/sql/import-h2.sql
+++ src/main/resources/sql/import-h2.sql
@@ -1,21 +1,18 @@
-
-INSERT INTO PRODUCT (product_id,name, description, quantity, price)
-VALUES (001,'Soap', 'Pears baby soap for Kids', 1, 35.75);
-INSERT INTO PRODUCT (product_id,name, description, quantity, price)
-VALUES (002,'Tooth Brush', 'Signal Tooth Brushes Size', 5, 34.50);
-INSERT INTO PRODUCT (product_id,name, description, quantity, price)
-VALUES (003,'Shirt', 'Casual Shirt imported from France', 3, 1500.00);
-INSERT INTO PRODUCT (product_id,name, description, quantity, price)
-VALUES (004,'Office Bag', 'Leather bag imported from USA', 40, 1000.00);
-INSERT INTO PRODUCT (product_id,name, description, quantity, price)
-VALUES (005,'Bottle', 'Hot Water Bottles', 80, 450.45);
-INSERT INTO PRODUCT (product_id,name, description, quantity, price)
-VALUES (006,'Wrist Watch', 'Imported wrist watches from swiss', 800, 2500.00);
-INSERT INTO PRODUCT (product_id,name, description, quantity, price)
-VALUES (007,'Mobile Phone', '3G/4G capability', 700, 45000.00);
-INSERT INTO PRODUCT (product_id,name, description, quantity, price)
-VALUES (008,'Shampoo', 'Head and Shoulders Shampoo', 500, 300.00);
-INSERT INTO PRODUCT (product_id,name, description, quantity, price)
-VALUES (009,'Leather Wallets', 'Imported Leather Wallets from AUS', 1000, 500.00);
-INSERT INTO PRODUCT (product_id,name, description, quantity, price)
-VALUES (010,'Camera', 'Imported Canon camera from USA', 10, 85000.00);
+INSERT INTO PRODUCTDEFAULTPRICE (product_id,name, description,price)
+VALUES (1,'Soap', 'Pears baby soap for Kids',35.00);
+INSERT INTO PRODUCTDEFAULTPRICE (product_id,name, description,price)
+VALUES (2,'Tooth Brush', 'Signal Tooth Brushes Size',34.00);
+INSERT INTO PRODUCTDEFAULTPRICE (product_id,name, description,price)
+VALUES (3,'Shirt', 'Casual Shirt imported from France', 1520.00);
+INSERT INTO PRODUCTDEFAULTPRICE (product_id,name, description,price)
+VALUES (4,'Office Bag', 'Leather bag imported from USA',1010.00);
+INSERT INTO PRODUCTDEFAULTPRICE (product_id,name, description,price)
+VALUES (5,'Bottle', 'Hot Water Bottles', 450.00);
+INSERT INTO PRODUCTDEFAULTPRICE (product_id,name, description,price)
+VALUES (6,'Wrist Watch', 'Imported wrist watches from swiss',2510.00);
+INSERT INTO PRODUCTDEFAULTPRICE (product_id,name, description,price)
+VALUES (7,'Mobile Phone', '3G/4G capability',44900.00);
+INSERT INTO PRODUCTDEFAULTPRICE (product_id,name, description,price)
+VALUES (8,'Shampoo', 'Head and Shoulders Shampoo',290.00);
+INSERT INTO PRODUCTDEFAULTPRICE (product_id,name, description,price)
+VALUES (9,'Leather Wallets', 'Imported Leather Wallets from AUS',510.00);
